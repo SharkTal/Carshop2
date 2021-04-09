f1a4ed60383741aecd24832d1e9534e522ee8dae
SharkTal <liutao.gao@gmail.com>
2021-04-09 13:45:51 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import Carlist from './component/Carlist';
+import './App.css';
+import Carlist from './components/Carlist';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
   return (
     <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            CarShop
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <Carlist />
     </div>
   );
 }
 
 export default App;
+ 
