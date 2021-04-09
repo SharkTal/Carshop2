a6773356f8d8f38c044b2fce14497970e65e431f
SharkTal <liutao.gao@gmail.com>
2021-04-09 13:26:59 +0300

    first

--- src/component/Carlist.js
+++ src/component/Carlist.js
@@ -1,39 +1,59 @@
 import React, { useEffect, useState } from 'react';
+
 import { AgGridReact } from 'ag-grid-react';
+
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
-const Carlist = () =>{
-    const [cars, setCars] = useState([]);
-    const fetchCars = () => {
-        fetch('http://carrestapi.herokuapp.com/cars')
-        .then(res => res.json())
-        .then(data => setCars(data._embedded.cars))
-        .catch(err => console.log(err))
-    }
+function Carlist() {
+  const [cars, setCars] = useState([]);
 
-    useEffect(() =>{
+  useEffect(() => {
+    fetchCars()
+  }, []);
+
+  const fetchCars = () => {
+    fetch('http://carrestapi.herokuapp.com/cars')
+    .then(response => response.json())
+    .then(data => setCars(data._embedded.cars))
+    .catch(err => console.error(err))
+  }
+
+  const deleteCar = (url) => {
+    fetch(url, { method: 'DELETE' })
+    .then(response => {
+      if (response.ok)
         fetchCars();
-    }, [])
+      else
+        alert('Something went wrong!');
+    })
+    .catch(err => console.error(err))
+  }
 
-    const columns = [
-        {field: 'brand'},
-        {field: 'model'},
-        {field: 'color'},
-        {field: 'fuel'},
-        {field: 'year'},
-        {field: 'price'},
+  const columns = [
+    { field: 'brand', sortable: true, filter: true },
+    { field: 'model', sortable: true, filter: true  },
+    { field: 'color', sortable: true, filter: true  },
+    { field: 'fuel', sortable: true, filter: true  },
+    { field: 'year', sortable: true, filter: true, width: 100  },
+    { field: 'prie', sortable: true, filter: true, width: 100  },
+    { 
+      headerName: '',
+      field: '_links.self.href',
+      cellRendererFramework: params => <button onClick={() => deleteCar(params.value)}>Delete</button>   
+    }
+  ];
 
-    ]
-
-    return (
-        <div className="ag-theme-material" style={{ height: 600, width: '90%' }}>
-            <AgGridReac
-                rowData={cars}
-                columnDefs={columns}
-            />
-        </div>
-    )
+  return(
+    <div className="ag-theme-material" style={{ height: 600, width: '90%', margin: 'auto' }}>
+      <AgGridReact
+        rowData={cars}
+        columnDefs={columns}
+        pagination={true}
+        paginationPageSize={10}
+      />
+    </div>
+  )
 }
 
 export default Carlist;
